8d92c22ffc5c5abbcf3378476ce195de6d7acee9
hezhaoyin <2561033753@qq.com>
2018-07-11 14:16:09 +0800

    fixed

--- index.js
+++ index.js
@@ -7,92 +7,92 @@ const path = require('path');
 const ejs = require('ejs');
 
 const {
-    handleImg
+  handleImg
 } = require('./handleImg');
 
 const SERVER_CONFIG = {
-    'basePath': 'images', //存放图片的目录
-    'port': 4000, //端口
-    'host': 'localhost'
+  'basePath': 'images', //存放图片的目录
+  'port': 4000, //端口
+  'host': 'localhost'
 }
 
 let server = http.createServer((req, res) => {
-    init(req, res)
+  init(req, res)
 })
 
 server.listen(SERVER_CONFIG.port, SERVER_CONFIG.host, () => {
-    console.log(`GetImage Server Running at http://${SERVER_CONFIG.host}:${SERVER_CONFIG.port}`)
+  console.log(`GetImage Server Running at http://${SERVER_CONFIG.host}:${SERVER_CONFIG.port}`)
 })
 
 
 let extNameList = {
-    '.jpeg': 'image/jpeg',
-    '.jpg': 'image/jpeg',
-    '.png': 'image/png',
-    '.gif': 'image/gif'
+  '.jpeg': 'image/jpeg',
+  '.jpg': 'image/jpeg',
+  '.png': 'image/png',
+  '.gif': 'image/gif'
 }
 
 function init(req, res) {
-    let urlObj = url.parse(req.url, true)
-    if (urlObj.pathname === '/favicon.ico') {
-        return;
+  let urlObj = url.parse(req.url, true)
+  if (urlObj.pathname === '/favicon.ico') {
+    return;
+  }
+  console.log(urlObj);
+  let imgExtname = path.extname(urlObj.pathname);
+  if (path.extname(req.url) === '') {
+    res.writeHead(200, {
+      'Content-Type': 'text/html;charset=utf-8'
+    });
+    let fullPath = path.join(__dirname, SERVER_CONFIG.basePath, urlObj.pathname)
+    if (!fs.existsSync(fullPath)) {
+      let str = fs.readFileSync('./public/404.ejs', 'utf-8');
+      let html = ejs.render(str, {
+        message: '找不到该目录',
+        version: process.version,
+        filename: __dirname
+      })
+      res.end(html)
+      return;
     }
-    console.log(urlObj);
-    let imgExtname = path.extname(urlObj.pathname);
-    if (path.extname(req.url) === '') {
+    let imgList = fs.readdirSync(fullPath);
+    console.log(imgList);
+    for (let i = 0; i < imgList.length; i++) {
+      imgList[i] = {
+        showUrl: 'http://' + SERVER_CONFIG.host + ':' + SERVER_CONFIG.port + '/' + imgList[i] + '?w=200',
+        fullUrl: 'http://' + SERVER_CONFIG.host + ':' + SERVER_CONFIG.port + '/' + imgList[i]
+      }
+    }
+    let str = fs.readFileSync('./public/index.ejs', 'utf-8');
+    let html = ejs.render(str, {
+      list: imgList,
+      filename: __dirname
+    })
+    res.end(html);
+  } else {
+    let params = urlObj.query;
+    fs.readFile(path.join(__dirname, SERVER_CONFIG.basePath, urlObj.pathname), function (err, myImage) {
+      if (!err) {
+        let newImage;
+        let bufImage = new Buffer(myImage);
+        let imageOption = {
+          w: Number(params.w),
+          h: Number(params.h),
+          o: Number(params.o) || 100,
+        }
+        newImage = handleImg(bufImage, imageOption.w, imageOption.h, imageOption.o, imgExtname)
         res.writeHead(200, {
-            'Content-Type': 'text/html;charset=utf-8'
+          'Content-Type': extNameList[imgExtname] + ';charset=utf-8'
         });
-        let fullPath = path.join(__dirname, SERVER_CONFIG.basePath, urlObj.pathname)
-        if(!fs.existsSync(fullPath)){
-          let str = fs.readFileSync('./public/404.ejs', 'utf-8');
-          let html = ejs.render(str, {
-            message: '找不到该目录',
-            version: process.version,
-            filename: __dirname
-          })
-          res.end(html)
-          return;
-        }
-        let imgList = fs.readdirSync(fullPath);
-        console.log(imgList);
-        for (let i = 0; i < imgList.length; i++) {
-            imgList[i] = {
-                showUrl:'http://' + SERVER_CONFIG.host + ':' + SERVER_CONFIG.port + '/' + imgList[i] + '?w=200',
-                fullUrl: 'http://' + SERVER_CONFIG.host + ':' + SERVER_CONFIG.port + '/' + imgList[i]
-            }
-        }
-        let str = fs.readFileSync('./public/index.ejs', 'utf-8');
+        res.end(newImage);
+      } else {
+        let str = fs.readFileSync('./public/404.ejs', 'utf-8');
         let html = ejs.render(str, {
-            list: imgList,
-            filename: __dirname
+          message: '找不到该文件',
+          version: process.version,
+          filename: __dirname
         })
-        res.end(html);
-    } else {
-        let params = urlObj.query;
-        fs.readFile(path.join(__dirname, SERVER_CONFIG.basePath, urlObj.pathname), function (err, myImage) {
-            if (!err) {
-                let newImage;
-                let bufImage = new Buffer(myImage);
-                let imageOption = {
-                    w: Number(params.w),
-                    h: Number(params.h),
-                    o: Number(params.o) || 100,
-                }
-                newImage = handleImg(bufImage, imageOption.w, imageOption.h, imageOption.o, imgExtname)
-                res.writeHead(200, {
-                    'Content-Type': extNameList[imgExtname] + ';charset=utf-8'
-                });
-                res.end(newImage);
-            } else {
-                let str = fs.readFileSync('./public/404.ejs', 'utf-8');
-                let html = ejs.render(str, {
-                  message: '找不到该文件',
-                  version: process.version,
-                  filename: __dirname
-                })
-                res.end(html)
-            }
-        });
-    }
+        res.end(html)
+      }
+    });
+  }
 }
